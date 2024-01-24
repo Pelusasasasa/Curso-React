7635fb5f8caea6047a5cbf58447e7d5406effe86
Pelusa-not <agustinlorenzatto@gmail.com>
2024-01-24 19:26:48 -0300

    Termine la seccion del testo

---
+++ 04-gif-expert-app/test/hooks/useFetchGifs.test.js
@@ -0,0 +1,28 @@
+import { renderHook, waitFor } from "@testing-library/react";
+import { useFetchGifs } from "../../src/hooks/useFetchGifs";
+
+describe('Puerbas en el hook de nuestro useFetchGifs', () => {
+
+    test('debe de regresar el estado inicial', () => { 
+        
+        const { result } = renderHook( () => useFetchGifs('One Punch') );
+        const {images, isLoading} = result.current;
+        expect(images.length).toBe(0);
+        expect(isLoading).toBe(true);
+
+     })
+
+     test('debe de regresar un arreglo deimages y el isloading false', async() => { 
+        
+        const { result } = renderHook( () => useFetchGifs('One Punch') );
+        await waitFor(
+            () => expect( result.current.images.length ).toBeGreaterThan(0)
+        );
+
+        const {images, isLoading} = result.current;
+        expect(images.length).toBe(10);
+        expect(isLoading).toBe(false);
+
+     })
+
+});
