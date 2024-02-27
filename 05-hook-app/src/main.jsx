d38f63928d6a766c3d0d9c9a5c3a0bda1c67c1b5
Pelusa-not <agustinlorenzatto@gmail.com>
2024-02-27 19:53:02 -0300

    Sigo con el curso

--- 05-hook-app/src/main.jsx
+++ 05-hook-app/src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
+import { TodoApp } from './08-useReducer/TodoApp'
 // import { HooksApp } from './HooksApp'
 // import { SimpleForm } from './useEffect/SimpleForm'
 // import { FormWithCustomHook } from './useEffect/FormWithCustomHook'
@@ -10,13 +11,15 @@ import './index.css'
 // import { Memorize } from './06-memos/Memorize'
 // import { MemoHook } from './06-memos/MemoHook'
 // import { CallbackHook } from './06-memos/CallbackHook'
-import { Padre } from './07-tarea-memo/Padre'
+// import { Padre } from './07-tarea-memo/Padre';
+// import './08-useReducer/intro-reducer';
 // import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook'
 // import { CounterApp } from './01-useState/CounterApp'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
-    <Padre />,
+    // <Padre />,
+    <TodoApp/>
   // </React.StrictMode>,
   
 )
