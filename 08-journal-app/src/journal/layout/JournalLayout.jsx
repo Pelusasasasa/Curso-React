6f8099370c03e3b4ea607b8a171d55b881a50755
Pelusa-not <agustinlorenzatto@gmail.com>
2024-07-23 11:45:00 -0300

    Empezando con el vanbar y el sidebar

--- 08-journal-app/src/journal/layout/JournalLayout.jsx
+++ 08-journal-app/src/journal/layout/JournalLayout.jsx
@@ -1,8 +1,23 @@
-import { Box } from "@mui/material"
+import { Box } from "@mui/material";
+import { Navbar, SideBar } from "../components";
 
-export const JournalLayout = () => {
+const drawerWidth = 240;
+
+export const JournalLayout = ({ children }) => {
   return (
     <Box sx={{ display: 'flex'}}>
+
+      <Navbar drawerWidth={ drawerWidth }/>
+
+      <SideBar drawerWidth = { drawerWidth } />
+
+      <Box 
+        component='main'
+        sx={{ flexGrow: 1, p: 3}}>
+
+          {children}
+
+      </Box>
 
     </Box>
   )
--- 08-journal-app/src/journal/pages/JournalPage.jsx
+++ 08-journal-app/src/journal/pages/JournalPage.jsx
@@ -1,7 +1,13 @@
 import { Typography } from "@mui/material"
+import { JournalLayout } from "../layout/JournalLayout"
 
 export const JournalPage = () => {
   return (
-    <Typography variant="h1">JournalApp</Typography>
+    <JournalLayout>
+      <Typography>
+         Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed odio omnis dolorem illo quidem reprehenderit quis atque ipsam at soluta nihil doloremque molestias veniam,
+         culpa neque iusto necessitatibus non totam.
+        </Typography>
+    </JournalLayout>
   )
 }
