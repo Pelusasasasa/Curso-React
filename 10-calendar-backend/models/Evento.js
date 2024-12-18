0a780030ba6e75012c93031ac211c8aee0806cd6
Pelusa-not <agustinlorenzatto@gmail.com>
2024-12-18 00:14:54 -0300

    Terminando seccion 24

--- 10-calendar-backend/models/Evento.js
+++ 10-calendar-backend/models/Evento.js
@@ -4,25 +4,30 @@ const Evento = new Schema({
 
     title: {
         type: String,
-        require: true
+        required: true
     },
     notes:{ 
         type: String
     },
     start:{
         type: Date,
-        require: true
+        required: true
     },
     end: {
         type: Date,
-        require: true
+        required: true
     },
     user: {
         type: Schema.Types.ObjectId,
-        ref: 'Usuario'
+        ref: 'Usuario',
+        required: true 
     }
 
 });
 
-
+Evento.method('toJSON', function(){
+     const {__v, _id, ...object} = this.toObject();
+     object.id = _id;
+     return object
+})
 module.exports = model('Evento', Evento);
